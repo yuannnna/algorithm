6d91665fcb883eca5c31dadf7a1465febffe0843
yuannnna <162145343+yuannnna@users.noreply.github.com>
2025-03-20 22:28:56 +0900

    [Silver IV] Title: 수 찾기, Time: 140 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cin >> n;
+
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	cin >> m;
+	vector<int> b(m);
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+	sort(a.begin(), a.end());
+
+	for (int i = 0; i < m; i++) {
+		if (binary_search(a.begin(), a.end(), b[i])) {
+			b[i] = 1;
+		}
+		else b[i] = 0;
+	}
+	for (int i = 0; i < m; i++) {
+		cout << b[i] << '\n';
+	}
+	return 0;
+}
